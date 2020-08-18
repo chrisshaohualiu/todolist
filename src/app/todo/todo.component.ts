953b889c3c68c6e9f6630ba3ba06b6189d7dd2b5
chris <chrisshaohualiu@gmail.com>
2020-08-18 17:01:22 -0400

    first commit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../interfaces/todo';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -23,7 +24,51 @@ export class TodoComponent implements OnInit {
     },
   ];
 
+  searchTerm: string;
+
+  showIndex: number;
+
   constructor() {}
+
+  removeTask(index: number) {
+    this.toDoList.splice(index, 1);
+  }
+
+  addTask(form: NgForm) {
+    let newTodo: Todo = {
+      task: form.value.task,
+      completed: false,
+    };
+    this.toDoList.push(newTodo);
+    form.reset();
+  }
+
+  completeTask(index: number) {
+    this.toDoList[index].completed = true;
+  }
+
+  setSearchTerm(form: NgForm) {
+    this.searchTerm = form.value.searchTerm.trim().toLowerCase();
+  }
+
+  filter() {
+    if (!this.searchTerm) {
+      return this.toDoList;
+    } else {
+      return this.toDoList.filter((item) => {
+        return item.task.toLowerCase().includes(this.searchTerm);
+      });
+    }
+  }
+
+  setShowIndex(i: number) {
+    this.showIndex = i;
+  }
+
+  resetShowIndex() {
+    this.showIndex = undefined;
+  }
+  editTask(form: NgForm, i: number) {}
 
   ngOnInit(): void {}
 }
